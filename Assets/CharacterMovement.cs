ffbf7a6b01500d962855141d6c96daae050b6166
Ben Mora <blackopsben@gmail.com>
2021-06-11 18:36:51 -0500

    Basic character movement

---
+++ Assets/CharacterMovement.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    [SerializeField] float speedToTask = 2f;
+    [SerializeField] float speedToRope = 1f;
+    [SerializeField] float speedMultiplier = 1f;
+    private float currentSpeed;
+
+    [SerializeField] Transform ropeDestination;
+    [SerializeField] Transform taskDestination;
+    private Transform currentDestination;
+
+    private void Start()
+    {
+        currentSpeed = speedToTask;
+        currentDestination = taskDestination;
+    }
+
+    private void Update()
+    {
+        UpdateSpeed();
+        Move();
+    }
+
+    private void UpdateSpeed()
+    {
+        if (currentDestination == taskDestination)
+        {
+            currentSpeed = speedToTask;
+        }
+        else
+        {
+            currentSpeed = speedToRope;
+        }
+    }
+
+    private void Move()
+    {
+        //float distToDest = Vector2.Distance(transform.position, currentDestination.position);
+        transform.position = Vector2.MoveTowards(transform.position, currentDestination.position, currentSpeed * speedMultiplier * Time.deltaTime);
+    }
+
+    public void GrabRope(bool value)
+    {
+        if (value)
+        {
+            currentDestination = ropeDestination;
+        }
+        else
+        {
+            currentDestination = taskDestination;
+        }
+    }
+}
--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -5,20 +5,27 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] CharacterMovement HackerMovement;
+    [SerializeField] CharacterMovement CleanerMovement;
+
     bool isGrabbingLeft;
     bool isGrabbingRight;
+
+    private void Update()
+    {
+        HackerMovement.GrabRope(isGrabbingLeft);
+        CleanerMovement.GrabRope(isGrabbingRight);
+    }
 
     public void OnGrabLeft(InputAction.CallbackContext context)
     {
         if (context.performed)
         {
             isGrabbingLeft = true;
-            Debug.Log("Grab Left Performed: " + isGrabbingLeft);
         }
         else if (context.canceled)
         {
             isGrabbingLeft = false;
-            Debug.Log("Grab Left Canceled: " + isGrabbingLeft);
         }
     }
 
@@ -27,12 +34,10 @@ public class PlayerController : MonoBehaviour
         if (context.performed)
         {
             isGrabbingRight = true;
-            Debug.Log("Grab Right Performed: " + isGrabbingRight);
         }
         else if (context.canceled)
         {
             isGrabbingRight = false;
-            Debug.Log("Grab Right Canceled: " + isGrabbingRight);
         }
     }
 }
