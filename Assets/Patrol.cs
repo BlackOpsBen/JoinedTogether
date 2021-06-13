844274a359f99125941e1e1dfb49ffd7834b8bb8
Ben Mora <blackopsben@gmail.com>
2021-06-13 01:48:29 -0500

    Guard walk animation

--- Assets/Patrol.cs
+++ Assets/Patrol.cs
@@ -6,6 +6,13 @@ public class Patrol : MonoBehaviour
 {
     [SerializeField] float moveSpeed = 1f;
 
+    private Animator animator;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
     private bool isWalking = true;
 
     private void Update()
@@ -14,6 +21,8 @@ public class Patrol : MonoBehaviour
         {
             transform.position = new Vector3(transform.position.x - moveSpeed * Time.deltaTime, transform.position.y, transform.position.z);
         }
+
+        animator.SetBool("isWalking", isWalking);
     }
 
     public void SetWalking(bool value)
